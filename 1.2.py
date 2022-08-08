23d627bc037bcb770fd5ed21604c4273b318c0f8
rozhyaroslav <rozhyaroslav@gmail.com>
2022-08-09 01:01:03 +0400

    Delete 1.2.py

--- 1.2.py
+++
@@ -1,5 +0,0 @@
-time=int(input("Введите время в секундах: "));
-hours=int(time/3600);
-minutes=int(time%3600/60);
-seconds=time%3600%60;
-print("Ваше переведенное время в формате ЧЧ:MM:CC: ", hours, ":", minutes,":",seconds);
