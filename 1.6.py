e0a2d3729888afd9ce542914cadd1812c2f79648
rozhyaroslav <rozhyaroslav@gmail.com>
2022-08-09 01:01:10 +0400

    Delete 1.6.py

--- 1.6.py
+++
@@ -1,9 +0,0 @@
-plus = int(input("Введите сумму выручки фирмы: "));
-minus = int(input("Введите сумму издержек фирмы: "));
-if plus>minus:
-    print("Прибыль!")
-    print("Рентабельность: ",(plus-minus)/plus)
-    sotrudniki = int(input("Введите количество сотрудников фирмы: "));
-    print("Прибыль фирмы на одного сотрудника: ", plus/sotrudniki)
-if plus<minus:
-    print("Убыток!")
