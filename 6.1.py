564d939a9f0045f7685f3fe0743bc10d5283a3a8
rozhyaroslav <rozhyaroslav@gmail.com>
2022-08-22 23:57:59 +0400

    Update 6.1.py

--- 6.1.py
+++ 6.1.py
@@ -1,7 +1,10 @@
 from time import sleep
 from datetime import datetime
+
 class TrafficLight:
-    _COLORS = {'красный': 7, 'жёлтый': 2, 'зеленый': 10}
+    _COLORS = {'красный': int(input("Введите длительность красного сигнала \n (по условию '7'): ")),
+               'жёлтый': int(input("Введите длительность желтого сигнала \n (по условию '2'): ")),
+               'зеленый': int(input("Введите длительность зеленого сигнала \n (по условию 'любое значение'): "))}
     _color = None
     def running(self):
         for COLOR, time_change in self._COLORS.items():
@@ -9,6 +12,9 @@ class TrafficLight:
             start_time = datetime.now()
             print("Выполнение:")
             print(f"На светофоре горит {self._color} на протяжении {time_change} секунд")
+            if (time_change != 7 and COLOR=='красный') or (time_change != 2 and COLOR=='жёлтый'):
+                print("Ошибка!!")
+                break
             sleep(time_change)
             print("Проверка:")
             print(f"На светофоре горел {self._color} на протяжении {(datetime.now() - start_time).seconds} секунд")
